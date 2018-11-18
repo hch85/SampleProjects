008b6854cef67626f3234d68bd47067038543098
Thinkpad <Thinkpad@Thinkpad-PC>
2018-11-18 16:45:55 -0500

    added sysout statement

--- FirstAntExample/src/com/ant/example/CalculatorTest.java
+++ FirstAntExample/src/com/ant/example/CalculatorTest.java
@@ -5,6 +5,7 @@ public class CalculatorTest {
 	public static void main(String[] args) {
 
 		Calculator calc = new Calculator();
+		System.out.println("Start Calculator");
 		System.out.println(calc.add(15, 20));
 		System.out.println(calc.subtract(15, 20));
 		System.out.println(calc.multiply(15, 20));
